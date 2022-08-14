ebaa676dd140362f13f63a6d31c965591f8fc575
Felipe Flor <f.flor@vehikl.com>
2022-08-14 19:14:19 -0400

    Organize game setup tests into group

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,22 +1,24 @@
 import {mount} from "@vue/test-utils"
 import App from "@/App.vue"
 
-it("accepts a 'rightAnswer' with 5 letters without giving any warnings", () => {
-    console.warn = jest.fn()
+describe("setting up the game", () => {
+    it("accepts a 'rightAnswer' with 5 letters without giving any warnings", () => {
+        console.warn = jest.fn()
 
-    const wrapper = mount(App, {props: {rightAnswer: "TESTS"}})
+        const wrapper = mount(App, {props: {rightAnswer: "TESTS"}})
 
-    expect(wrapper.vm).toBeTruthy()
-    expect(console.warn).not.toHaveBeenCalled()
-})
-
-it("provides a warning if the rightAnswer given does not have exactly 5 letters", () => {
-    let hasWarnedAboutWordProp = false
-    console.warn = jest.fn().mockImplementation((vueWarning) => {
-        hasWarnedAboutWordProp = /invalid.*prop.*rightAnswer/ig.test(vueWarning)
+        expect(wrapper.vm).toBeTruthy()
+        expect(console.warn).not.toHaveBeenCalled()
     })
 
-    mount(App, {props: {rightAnswer: "SomethingLargerThan5"}})
+    it("provides a warning if the rightAnswer given does not have exactly 5 letters", () => {
+        let hasWarnedAboutWordProp = false
+        console.warn = jest.fn().mockImplementation((vueWarning) => {
+            hasWarnedAboutWordProp = /invalid.*prop.*rightAnswer/ig.test(vueWarning)
+        })
 
-    expect(hasWarnedAboutWordProp).toBe(true)
+        mount(App, {props: {rightAnswer: "SomethingLargerThan5"}})
+
+        expect(hasWarnedAboutWordProp).toBe(true)
+    })
 })
