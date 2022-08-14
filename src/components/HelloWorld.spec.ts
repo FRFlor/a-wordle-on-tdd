b7028e89802c5a9cb15172f841a8021d5b37c997
Felipe Flor <f.flor@vehikl.com>
2022-08-14 18:21:08 -0400

    Remove clutter

--- src/components/HelloWorld.spec.ts
+++
@@ -1,9 +0,0 @@
-import {mount} from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
-
-it('renders the message that is given', () => {
-    const message = "Lets practice TDD!"
-    const wrapper = mount(HelloWorld, {props: {msg: message}})
-
-    expect(wrapper.text()).toContain(message)
-})
