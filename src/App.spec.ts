5659fca8e0945bf12f7b4304753751f599de5972
Felipe Flor <f.flor@vehikl.com>
2022-08-14 18:59:22 -0400

    Display warning when the rightAnswer given has more than 5 letters

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -5,3 +5,17 @@ it("accepts a 'rightAnswer'", () => {
     const wrapper = mount(App, {props: {rightAnswer: "TESTS"}})
     expect(wrapper.vm).toBeTruthy()
 })
+
+it("provides a warning if the rightAnswer given does not have exactly 5 letters", () => {
+    // Arrange
+    let hasWarnedAboutWordProp = false
+    console.warn = jest.fn().mockImplementation((vueWarning) => {
+        hasWarnedAboutWordProp = /invalid.*prop.*rightAnswer/ig.test(vueWarning)
+    })
+
+    // Act
+    mount(App, {props: {word: "SomethingLargerThan5"}})
+
+    // Assert
+    expect(hasWarnedAboutWordProp).toBe(true)
+})
--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,8 @@
   <p>Nothing :(</p>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import {onMounted} from "vue"
+
+onMounted(() => console.warn("Invalid Prop 'rightAnswer' was given!"))
 </script>
