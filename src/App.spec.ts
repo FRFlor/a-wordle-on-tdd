227bc5765864cd3ec0487a861ac86c6b73898ebd
Felipe Flor <f.flor@vehikl.com>
2022-08-14 22:07:09 -0400

    Reduce duplication of mount in tests

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,48 +1,50 @@
-import {mount} from "@vue/test-utils"
+import {mount, VueWrapper} from "@vue/test-utils"
 import App from "@/App.vue"
 
-describe("setting up the game", () => {
-    it("accepts a 'rightAnswer' with 5 letters without giving any warnings", () => {
-        console.warn = jest.fn()
+const rightAnswer = "TESTS"
+describe("Wordle", () => {
+    let wrapper: VueWrapper
 
-        const wrapper = mount(App, {props: {rightAnswer: "TESTS"}})
-
-        expect(wrapper.vm).toBeTruthy()
-        expect(console.warn).not.toHaveBeenCalled()
+    beforeEach(() => {
+        wrapper = mount(App, {props: {rightAnswer}})
     })
 
-    it("provides a warning if the rightAnswer given does not have exactly 5 letters", () => {
-        let hasWarnedAboutWordProp = false
-        console.warn = jest.fn().mockImplementation((vueWarning) => {
-            hasWarnedAboutWordProp = /invalid.*prop.*rightAnswer/ig.test(vueWarning)
+    describe("setting up the game", () => {
+        it("accepts a 'rightAnswer' with 5 letters without giving any warnings", () => {
+            console.warn = jest.fn()
+
+            const wrapper = mount(App, {props: {rightAnswer}})
+
+            expect(wrapper.vm).toBeTruthy()
+            expect(console.warn).not.toHaveBeenCalled()
         })
 
-        mount(App, {props: {rightAnswer: "SomethingLargerThan5"}})
+        it("provides a warning if the rightAnswer given does not have exactly 5 letters", () => {
+            let hasWarnedAboutWordProp = false
+            console.warn = jest.fn().mockImplementation((vueWarning) => {
+                hasWarnedAboutWordProp = /invalid.*prop.*rightAnswer/ig.test(vueWarning)
+            })
 
-        expect(hasWarnedAboutWordProp).toBe(true)
-    })
-})
+            mount(App, {props: {rightAnswer: "SomethingLargerThan5"}})
 
-describe("ending the game", () => {
-    it("does not display game ending messages until the game is over", () => {
-        const wrapper = mount(App, {props: {rightAnswer: "TESTS"}})
-
-        expect(wrapper.find("[data-role=winning-message]").exists()).toBe(false)
-        expect(wrapper.find("[data-role=losing-message]").exists()).toBe(false)
+            expect(hasWarnedAboutWordProp).toBe(true)
+        })
     })
 
-    it("displays a victory message when the player guesses the right word correctly", async () => {
-        // Arrange: Instantiate the App
-        const wrapper = mount(App, {props: {rightAnswer: "TESTS"}})
+    describe("ending the game", () => {
+        it("does not display game ending messages until the game is over", () => {
+            expect(wrapper.find("[data-role=winning-message]").exists()).toBe(false)
+            expect(wrapper.find("[data-role=losing-message]").exists()).toBe(false)
+        })
 
-        // Act: Type the correct guess and submit
-        await wrapper.find("[data-role=guess]").setValue("TESTS")
-        await wrapper.find("[data-role=guess]").trigger("keydown.enter")
+        it("displays a victory message when the player guesses the right word correctly", async () => {
+            await wrapper.find("[data-role=guess]").setValue(rightAnswer)
+            await wrapper.find("[data-role=guess]").trigger("keydown.enter")
 
-        // Assert: Verify that the winning message is in display
-        expect(wrapper.find("[data-role=winning-message]").exists()).toBe(true)
+            expect(wrapper.find("[data-role=winning-message]").exists()).toBe(true)
+        })
+
+        it.todo("displays a failure message when the player guesses wrong")
     })
-
-    it.todo("displays a failure message when the player guesses wrong")
 })
 
